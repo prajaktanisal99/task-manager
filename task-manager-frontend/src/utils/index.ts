5a526c33a4d3502a022ed719318fd31db1fe0bd8
prajaktanisal99 <prajaktanisal99@gmail.com>
2025-04-24 17:33:42 -0500

    frontend: cosmetic fix - add capitalize first letter util

--- task-manager-frontend/src/utils/index.ts
+++ task-manager-frontend/src/utils/index.ts
@@ -2,3 +2,4 @@ export * from "./get-formatted-date";
 export * from "./transform-tasks-to-columns";
 export * from "./get-column-title";
 export * from "./get-filtered-tasks";
+export * from "./capitalize-first-letter";
