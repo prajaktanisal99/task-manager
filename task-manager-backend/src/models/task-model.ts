030a00b433c369daf8aa3b2058f18e36aa6a7ee8
prajaktanisal99 <prajaktanisal99@gmail.com>
2025-04-24 17:39:20 -0500

    backend: update Task model

--- task-manager-backend/src/models/task-model.ts
+++ task-manager-backend/src/models/task-model.ts
@@ -1,35 +1,19 @@
 import mongoose from "mongoose";
 
-// export enum TaskPriority {
-// 	HIGH = "High",
-// 	MEDIUM = "Medium",
-// 	LOW = "Low",
-// }
-
-// export interface TaskType {
-// 	id: string;
-// 	title: string;
-// 	description: string;
-// 	priority: TaskPriority;
-// 	date: Date;
-// }
-
 const taskSchema = new mongoose.Schema({
 	title: { type: String, required: true },
 	description: { type: String },
 	priority: {
 		type: String,
 		enum: ["low", "medium", "high"],
-		// required: true,
 		default: "low",
 	},
 	status: {
 		type: String,
 		enum: ["todo", "inProgress", "done"],
-		// required: true,
 		default: "todo",
 	},
-	date: { type: Date, default: Date.now },
+	dueDate: { type: Date, default: Date.now },
 	order: { type: Number, required: true },
 });
 
