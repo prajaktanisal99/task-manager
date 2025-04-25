37172f9956752d01f63a4242a660fee15554b92d
prajaktanisal99 <prajaktanisal99@gmail.com>
2025-04-24 20:15:03 -0500

    frontend: change action bar styles

--- task-manager-frontend/src/components/task-action-bar.tsx
+++ task-manager-frontend/src/components/task-action-bar.tsx
@@ -17,34 +17,37 @@ export const TaskActionBar = () => {
 	return (
 		<div className="task-action-bar">
 			<input
-				className="task-search-input"
+				className="task-action-search"
 				placeholder="Search Tasks"
 				value={searchKey}
 				onChange={(e) => setSearchKey(e.target.value)}
 			></input>
 
 			<div className="task-action-buttons">
-				<div className="task-action-dates">
-					<div className="task-action-dates-label">
-						<label>Due by</label>
+				<div className="task-action-dates-priority">
+					<div className="task-action-dates">
+						<div className="task-action-dates-label">
+							<label>Due by</label>
+						</div>
+						<div>
+							<input
+								type="date"
+								value={dueByDate}
+								onChange={(e) => setDueByDate(e.target.value)}
+							/>
+						</div>
 					</div>
-					<div>
-						<input
-							type="date"
-							value={dueByDate}
-							onChange={(e) => setDueByDate(e.target.value)}
-						/>
-					</div>
+					<select
+						className="task-action-priority"
+						value={selectedPriority}
+						onChange={(e) => setSelectedPriority(e.target.value as TaskPriority)}
+					>
+						<option value={TaskPriority.ALL}>All Priorities</option>
+						<option value={TaskPriority.LOW}>Low</option>
+						<option value={TaskPriority.MEDIUM}>Medium</option>
+						<option value={TaskPriority.HIGH}>High</option>
+					</select>
 				</div>
-				<select
-					value={selectedPriority}
-					onChange={(e) => setSelectedPriority(e.target.value as TaskPriority)}
-				>
-					<option value={TaskPriority.ALL}>All Priorities</option>
-					<option value={TaskPriority.LOW}>Low</option>
-					<option value={TaskPriority.MEDIUM}>Medium</option>
-					<option value={TaskPriority.HIGH}>High</option>
-				</select>
 				<button
 					className="add-task-button"
 					onClick={() => {
